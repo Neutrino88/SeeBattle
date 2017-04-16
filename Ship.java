676fb1faaac26b9257331382f443dc594d4c22dd
u1 <u1>
2017-04-16 18:47:15 +0300

    ad some methods in Ship class

--- Ship.java
+++ Ship.java
@@ -1,7 +1,39 @@
 public class Ship {
-    private boolean status;
+    private boolean dead;
     private boolean orientation; // false=hor, true=ver
     private int length;
     private boolean[] parts;
 
+    public Ship(boolean orientation, int length){
+        this.dead = false;
+        this.length = length;
+        this.orientation = orientation;
+
+        this.parts = new boolean[length];
+        for (int i = 0; i < length; i++){
+            this.parts[i] = false;
+        }
+    }
+
+    public boolean doShot(int part){
+        // if part is uncorrect
+        if (part >= length || part < 0){
+            return false;
+        }
+        // update this.parts
+        this.parts[part] = true;
+
+        // update this.dead
+        boolean dead = this.parts[0];
+        for (int i = 1; i < this.length; i++){
+            dead = this.parts[i] & dead;
+        }
+        this.dead = dead;
+
+        return true;
+    }
+
+    public boolean isDead(){
+        return this.dead;
+    }
 }
